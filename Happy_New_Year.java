62e0fd03170665a927fad46d8e176d56b1f14650
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-06-01 09:17:04 +0530

    Initial commit

--- Happy_New_Year.java
+++ Happy_New_Year.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Exercise_And_Rest
+public class Happy_New_Year
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner input = new Scanner(System.in);
-	    int N = input.nextInt();
-	    System.out.print(N*3);
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		System.out.print(24-x);
 
 	}
 }
---
+++ Time_Penalty.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static ArrayList<Integer> valEqualToPos(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == i + 1) {
+                ans.add(arr[i]);
+            }
+        }
+        
+        return ans;
+    }
+}
