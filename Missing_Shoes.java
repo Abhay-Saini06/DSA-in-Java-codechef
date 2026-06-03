c26dac3b0cee08fc67119d80ffc20b91bb0a6e8b
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-06-03 22:09:36 +0530

    Missing_Shoes

--- Missing_Shoes.java
+++ Missing_Shoes.java
@@ -2,18 +2,14 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Assigment_Due
+public class Missing_Shoes
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner input = new Scanner(System.in);
-		int X = input.nextInt();
-		int Y = input.nextInt();
-		if(X>Y){
-		    System.out.print("NO");
-		}else{
-		    System.out.print("Yes");
-		}
+		int L = input.nextInt();
+		int R = input.nextInt();
+		System.out.print(Math.abs(L-R));
 
 	}
 }
