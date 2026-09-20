8d32af71f9b88663b661cb0ed02d24ab30cadeed
Abhay Saini <siddsaini0906@gmail.com>
2026-09-20 23:46:39 +0530

    Add Teleport Home program to calculate distance

---
+++ Teleport Home.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int D = sc.nextInt();
+		int T = sc.nextInt();
+		
+		System.out.println(Math.max(0, D - T));
+	}
+}
