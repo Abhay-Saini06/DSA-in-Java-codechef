31895b89b232d62f446c2cdac9fadaaf47631a56
Abhay Saini <siddsaini0906@gmail.com>
2026-09-21 09:39:04 +0530

    Create Missing Number.java

---
+++ Basic_Problems/Missing Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int S = input.nextInt();
+		System.out.print(10-S);
+
+	}
+}
