45aef05135d31130a6a68edcd2d8d8a6d44cf4ec
Abhay Saini <siddsaini0906@gmail.com>
2026-09-21 10:49:04 +0530

    Create Cloud Watching.java

---
+++ Basic_Problems/Cloud Watching.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
+		if(B>=A*3){
+		    System.out.print("Rain");
+		}else{
+		    System.out.print("Dry");
+		}
+
+	}
+}
