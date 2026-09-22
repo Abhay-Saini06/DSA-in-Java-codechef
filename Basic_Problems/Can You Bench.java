ca8773fda2f920b094ff7e3473ab99fa964a59ea
Abhay Saini <siddsaini0906@gmail.com>
2026-09-22 12:14:35 +0530

    Add Can You Bench.java for bench calculation

---
+++ Basic_Problems/Can You Bench.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		int bench = X+20;
+		System.out.print(bench);
+
+	}
+}
