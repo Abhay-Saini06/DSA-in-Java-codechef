cb3940a3057e78577c83f5cec95571a89c6fc491
Abhay Saini <siddsaini0906@gmail.com>
2026-09-22 12:29:01 +0530

    Add Contest Registration Fee Java program

---
+++ Basic_Problems/Contest Registration Fee.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		int Y = input.nextInt();
+		if(Y<=X){
+		    System.out.print("0");
+		}else{
+		    System.out.print("100");
+		}
+
+	}
+}
