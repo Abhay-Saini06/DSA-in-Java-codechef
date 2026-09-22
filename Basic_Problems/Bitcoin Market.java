ada79ba628d62ed92600dbbace8c531a68da82c2
Abhay Saini <siddsaini0906@gmail.com>
2026-09-22 12:25:48 +0530

    Create Bitcoin Market.java

---
+++ Basic_Problems/Bitcoin Market.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int R = input.nextInt();
+		if(R<=4){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");
+		}
+	}
+}
