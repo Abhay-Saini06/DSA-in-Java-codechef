c3fe52716f5d7ac1f5d18104e67457cc79536b19
Abhay Saini <siddsaini0906@gmail.com>
2026-09-23 10:43:12 +0530

    Create Entry Check.java

---
+++ Basic_Problems/Entry Check.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		if(X>=10){
+		    System.out.print("YES");
+		}else{
+		    System.out.print("NO");
+		}
+	}
+}
