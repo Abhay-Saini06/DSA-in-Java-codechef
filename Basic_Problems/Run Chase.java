a45e7b893200def2c05d86e8e7abc296387ff8f2
Abhay Saini <siddsaini0906@gmail.com>
2026-09-23 10:35:45 +0530

    Create Run Chase.java

---
+++ Basic_Problems/Run Chase.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        int R = N / 20 + 1;
+
+        System.out.println(R);
+    }
+}
