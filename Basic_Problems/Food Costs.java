39572ea4932a1b80cfe2c70e841b2442b9cac9a3
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 15:37:40 +0530

    Create Food Costs.java

---
+++ Basic_Problems/Food Costs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input  = new Scanner(System.in);
+		int X = input.nextInt();
+		int Y = input.nextInt();
+		int mess = X*6;
+		int result = Y+mess;
+		System.out.println(result);
+	}
+}
