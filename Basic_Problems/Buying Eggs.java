d353ce9916b0060380b9a66e4c2e8562d9d1e71b
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 15:28:16 +0530

    Create Buying Eggs.java

---
+++ Basic_Problems/Buying Eggs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner input = new Scanner(System.in);
+
+        int X = input.nextInt();
+        int Y = input.nextInt();
+        int F = input.nextInt();
+
+        int first = X * 12;
+        int second = Y * 12 + F;
+
+        System.out.println(Math.min(first, second));
+    }
+}
