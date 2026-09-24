d6c29948b2c91a72e6185d97eaeb71f4b8e185c9
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 15:45:35 +0530

    Create Moneymaking.java

---
+++ Basic_Problems/Moneymaking.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		int Y = input.nextInt();
+		int X1 = X*5000;
+		int Y1 = Y*9800;
+		int result = X1+Y1;
+		System.out.print(result);
+
+	}
+}
