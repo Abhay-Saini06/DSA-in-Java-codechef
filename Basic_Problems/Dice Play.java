e8f25895c3970ba289ddaaf5f4132513180487e6
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 17:26:45 +0530

    Create Dice Play.java

---
+++ Basic_Problems/Dice Play.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
+		if(A==B){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+	}
+}
