616efc7057b3442615c6fe444d8a0223f3cbd495
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 14:58:29 +0530

    Add Spring Cleaning program to Basic Problems

---
+++ Basic_Problems/Spring Cleaning.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		int Y = input.nextInt();
+		int xtime = 30*X;
+		int ytime = 60*Y;
+		System.out.println(xtime+ytime);
+		
+	}
+}
