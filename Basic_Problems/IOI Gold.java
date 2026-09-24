1c1bc190d9a929935ace24adf38117517dde401b
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 15:33:35 +0530

    Add IOI Gold.java for input comparison

---
+++ Basic_Problems/IOI Gold.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
+		int G = input.nextInt();
+		if(N>=G){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+	}
+}
