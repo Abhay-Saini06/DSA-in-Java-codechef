9881e66f8fd1b003f62afc04e218ee9704ef3d1e
Abhay Saini <siddsaini0906@gmail.com>
2026-09-24 15:24:05 +0530

    Add Time Machine.java for Codechef problem

---
+++ Basic_Problems/Time Machine.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int X = input.nextInt();
+		if(2050-X<=25){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+	}
+}
